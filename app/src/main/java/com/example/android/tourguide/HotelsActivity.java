47fc151b1c9cae5169165d52362174fb0acfb744
sun-strider <mevola8@gmail.com>
2017-06-30 18:52:26 +0200

    Added all Hotel drawables with different scale sizes
    Added new icon. Made this icon the default mock image in list_item.xml

--- app/src/main/java/com/example/android/tourguide/HotelsActivity.java
+++ app/src/main/java/com/example/android/tourguide/HotelsActivity.java
@@ -20,10 +20,10 @@ public class HotelsActivity extends AppCompatActivity {
         // Create a list of locations
         final ArrayList<Location> locations = new ArrayList<Location>();
         locations.add(new Location(getString(R.string.title_01), getString(R.string.description_01), getString(R.string.address_01), R.drawable.piazza));
-        locations.add(new Location(getString(R.string.title_02), getString(R.string.description_02), getString(R.string.address_02), R.drawable.number_one));
-        locations.add(new Location(getString(R.string.title_03), getString(R.string.description_03), getString(R.string.address_03), R.drawable.number_one));
-        locations.add(new Location(getString(R.string.title_04), getString(R.string.description_04), getString(R.string.address_04), R.drawable.number_one));
-        locations.add(new Location(getString(R.string.title_05), getString(R.string.description_05), getString(R.string.address_05), R.drawable.number_one));
+        locations.add(new Location(getString(R.string.title_02), getString(R.string.description_02), getString(R.string.address_02), R.drawable.seven_boutique));
+        locations.add(new Location(getString(R.string.title_03), getString(R.string.description_03), getString(R.string.address_03), R.drawable.elvezia));
+        locations.add(new Location(getString(R.string.title_04), getString(R.string.description_04), getString(R.string.address_04), R.drawable.casa_oliva));
+        locations.add(new Location(getString(R.string.title_05), getString(R.string.description_05), getString(R.string.address_05), R.drawable.antica_posta));
 
 
         // Create an {@link LocationAdapter}, whose data source is a list of {@link Location}s. The
